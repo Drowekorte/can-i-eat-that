de9cb4f3ae1f6348cffad038d6ff3e86c9fb95f2
Drowekorte <drowekorte@gmail.com>
2021-04-30 11:40:47 -0700

    k

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-async function 
+async function loginUser(credentials) {
+    return fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
 
 export default function Login({ setToken }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
     return (
         <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)}/>
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)}/>
-        </label>
-        <div>
-          <button type="submit">Submit</button>
+            <h1>Please Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Username</p>
+                    <input type="text" onChange={e => setUserName(e.target.value)} />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" onChange={e => setPassword(e.target.value)} />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
     )
 }
 
 Login.propTypes = {
     setToken: PropTypes.func.isRequired
-  }
+}
