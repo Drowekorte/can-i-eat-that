dbe5b70481b1a0c54769e9d088334835cdeb0e06
EmaCartoon <emacartoon@gmail.com>
2021-05-05 11:14:24 -0700

    Added pseudo code to search sections

--- client/src/components/SearchBoxes/searchboxes.js
+++ client/src/components/SearchBoxes/searchboxes.js
@@ -7,23 +7,27 @@ class SearchBoxes extends React.Component {
     state = { searchOptions };
 
   constructSrch = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
+    // Map this.state.searchOptions for checkboxes to display
+
+    // Filter to creat a new array of the selected boxes to add to the search URL
     const searchOptions = this.state.searchOptions.filter(srchOpt => srchOpt.id !== id);
-    // Set this.state.friends equal to the new friends array
+    // Set this.state.searchOptions equal to the new search array
     this.setState({ searchOptions });
   };
 
+// One button to unselect all or clear choices. Adding all wouldn't be helpful, so just clear.
+// <input
+//   type="checkbox"
+//   onChange={this.handleAllChecked}
+//    value="checkedall"
+//  />{" "}
+//  Check / Uncheck All 
+
   render() {
-      const { searchOptions } = this.state;
     return (
       <div className="Search">
         <h2>Search by Diet and Health Labels</h2>
-        <input
-          type="checkbox"
-          onChange={this.handleAllChecked}
-          value="checkedall"
-        />{" "}
-        Check / Uncheck All
+
         <ul>
           {this.state.searchOptions.map(srchOpt => (
               <CheckBox
