d933e3ab75e671b51f1a65e039dc273d25a989f5
EmaCartoon <emacartoon@gmail.com>
2021-05-05 14:18:45 -0700

    Checkboxes still need to be formatted, but they work. Need to add +prefix +value to each of the selected checkboxes onto the search URL.

--- client/src/components/SearchBoxes/searchboxes.js
+++ client/src/components/SearchBoxes/searchboxes.js
@@ -4,35 +4,42 @@ import "./searchboxes.css";
 import searchOptions from "./searchoptions.json";
 
 class SearchBoxes extends Component {
-    state = { searchOptions };
+  state = { searchOptions };
 
-  constructSrch = id => {
+  constructSrch = (id) => {
     // Map this.state.searchOptions for checkboxes to display
     // Filter to creat a new array of the selected boxes to add to the search URL
-    const searchOptions = this.state.searchOptions.filter(srchOpt => srchOpt.id !== id);
+    const searchOptions = this.state.searchOptions.filter(
+      (srchOpt) => srchOpt.id !== id
+    );
     // Set this.state.searchOptions equal to the new search array
     this.setState({ searchOptions });
   };
 
-// One button to unselect all or clear choices. Adding all wouldn't be helpful, so just clear.
-// <input
-//   type="checkbox"
-//   onChange={this.handleAllChecked}
-//    value="checkedall"
-//  />{" "}
-//  Check / Uncheck All 
+  // One button to unselect all or clear choices. Adding all wouldn't be helpful, so just clear.
+  // <input
+  //   type="checkbox"
+  //   onChange={this.handleAllChecked}
+  //    value="checkedall"
+  //  />{" "}
+  //  Check / Uncheck All
 
   render() {
     return (
-        <ul>
-          {this.state.searchOptions.map(srchOpt => (
-              <CheckBox
-                key={srchOpt.id}
-                name={srchOpt.name}
-              />
-            )
-          )}
-        </ul>
+      <div>
+      <ul>
+        {this.state.searchOptions.map((srchOpt) => (
+          <CheckBox
+            key={srchOpt.id}
+            name={srchOpt.name}
+            checked={srchOpt.isChecked}
+            value={srchOpt.value}
+            prefix={srchOpt.prefix}
+            tooltip={srchOpt.tooltip}
+          />
+        ))}
+      </ul>
+      </div>
     );
   }
 }
